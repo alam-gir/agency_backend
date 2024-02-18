392801cf2d31d8bcd468ce6c54e9caa1fe21061e
alam-gir <info.alamgirhussain@gmail.com>
2024-02-19 00:02:21 +0600

    origin added in cors option with www

--- src/utils/corsOptions.ts
+++ src/utils/corsOptions.ts
@@ -1,5 +1,5 @@
 import { CorsOptions } from "cors";
-const whitlists = ["http://localhost:3000", "http://localhost:3001","https://agency-frontend-mocha.vercel.app/", "https://wafipix.com", "http://wafipix.com"];
+const whitlists = ["http://localhost:3000", "http://localhost:3001","https://agency-frontend-mocha.vercel.app/", "https://www.wafipix.com", "https://wafipix.com", "http://wafipix.com"];
 export const corsOptions: CorsOptions = {
   origin: (origin, callback) => {
     if (whitlists.indexOf(origin!) !== -1 || !origin) {
