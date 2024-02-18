d6d8a00f436ffaefb165e769aecf6807382460a9
alam-gir <info.alamgirhussain@gmail.com>
2024-02-18 22:50:30 +0600

    remove nodes cluster from index

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import dotenv from "dotenv";
 import { connectDB } from "./utils/db";
 import http from "http";
 import cluster from "node:cluster";
-import os from "os";
+// import os from "os";
 import { Server } from "socket.io";
 
 dotenv.config({});
 
-const numCPUs = os.cpus().length;
+// const numCPUs = os.cpus().length;
 
 const server = http.createServer(app);
 
@@ -29,19 +29,19 @@ io.on("connection", (socket) => {
 const port = process.env.PORT;
 connectDB()
   .then((db) => {
-    if (cluster.isPrimary) {
-      console.log(`Primary ${process.pid} is running`);
-      for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-      }
-      cluster.on("exit", (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`);
-      });
-    } else {
-      const serverConnection = server.listen(port, () => {
-        console.log("Server run at port -> ", port);
-      });
-      serverConnection.timeout = 60000 * 5;
-    }
+    // if (cluster.isPrimary) {
+    //   console.log(`Primary ${process.pid} is running`);
+    //   for (let i = 0; i < numCPUs; i++) {
+    //     cluster.fork();
+    //   }
+    //   cluster.on("exit", (worker, code, signal) => {
+    //     console.log(`worker ${worker.process.pid} died`);
+    //   });
+    // } else {
+    const serverConnection = server.listen(port, () => {
+      console.log("Server run at port -> ", port);
+    });
+    serverConnection.timeout = 60000 * 5;
+    // }
   })
   .catch((error) => console.log(error.message));
