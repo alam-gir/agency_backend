9afa9a48e8f8e424c053b9b2960fe041efe438f9
alam-gir <info.alamgirhussain@gmail.com>
2024-02-21 23:26:16 +0600

    github provider sign in added

--- src/lib/providers.ts
+++ src/lib/providers.ts
@@ -51,16 +51,19 @@ export const providers = {
         grant_type: "authorization_code",
       };
 
-      const response = await fetch(`https://graph.facebook.com/v13.0/oauth/access_token?client_id=${data.client_id}&redirect_uri=${data.redirectUri}&client_secret=${data.client_secret}&code=${code}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams(data),
-      });
+      const response = await fetch(
+        `https://graph.facebook.com/v13.0/oauth/access_token?client_id=${data.client_id}&redirect_uri=${data.redirectUri}&client_secret=${data.client_secret}&code=${code}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: new URLSearchParams(data),
+        }
+      );
 
       const result = await response.json();
-      console.log({result})
+      console.log({ result });
 
       return result.access_token;
     },
@@ -70,47 +73,65 @@ export const providers = {
       );
 
       const user = await response.json();
-      console.log({user})
+      console.log({ user });
 
       return user;
-    }
+    },
   },
   github: {
     getAuthLink: (redirectUri: string) => {
-      const scope = encodeURIComponent(
-        "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
-      );
-      return `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
+      const scope = encodeURIComponent("user:email read:user");
+      return `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
     },
     getAccessToken: async ({ code }: { code: string }) => {
       const data: Record<string, string> = {
-        client_id: process.env.GOOGLE_CLIENT_ID || "",
-        client_secret: process.env.GOOGLE_CLIENT_SECRET || "",
+        client_id: process.env.GITHUB_CLIENT_ID || "",
+        client_secret: process.env.GITHUB_CLIENT_SECRET || "",
         code,
-        redirectUri: `${process.env.BASE_API_V1_URL}/auth/login/google/callback`,
+        redirectUri: `${process.env.BASE_API_V1_URL}/auth/login/github/callback`,
         grant_type: "authorization_code",
       };
 
-      const response = await fetch(`https://oauth2.googleapis.com/token`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams(data),
-      });
+      const response = await fetch(
+        `https://github.com/login/oauth/access_token?client_id=${data.client_id}&client_secret=${data.client_secret}&code=${code}&redirect_uri=${data.redirectUri}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        }
+      );
 
       const result = await response.json();
-
+      console.log({ result });
       return result.access_token;
     },
     getUserInfo: async (access_token: string) => {
-      const response = await fetch(
-        `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`
+      let user = await fetch(
+        `https://api.github.com/user?access_token=${access_token}`,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
       );
 
-      const user = await response.json();
+      user = await user.json();
 
-      return user;
+      let email = await fetch(
+        `https://api.github.com/user/emails?access_token=${access_token}`,
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+
+      email = await email.json();
+      email = (email as any).filter((e: any) => e.primary)[0].email;
+      
+      return{name: (user as any).name, email, avatar: (user as any).avatar_url} ;
     },
   },
 };
